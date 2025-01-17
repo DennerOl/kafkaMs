665b3451590502c33cbed2a289313f65b954777f
DennerOl <dener155@hotmail.com>
2025-01-17 19:44:11 -0300

    enviando e recebendo dados orderService

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/consumer/EventConsumer.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/consumer/EventConsumer.java
@@ -3,6 +3,7 @@ package br.com.microservices.orchestrated.orderservice.core.consumer;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import br.com.microservices.orchestrated.orderservice.core.service.EventService;
 import br.com.microservices.orchestrated.orderservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,10 +15,13 @@ public class EventConsumer {
 
     private final JsonUtil jsonUtil;
 
+    private final EventService service;
+
     @KafkaListener(groupId = "${spring.kafka.consumer.group-id}", topics = "${spring.kafka.topic.notify-ending}")
     public void consumeNotifyEndingEvent(String payload) {
         log.info("Recebendo evento de notificação final {} de notify-ending topic", payload);
         var event = jsonUtil.toEvent(payload);
+        service.notifyEnding(event);
 
     }
 }
